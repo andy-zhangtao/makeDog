98a0ec3981bd3c48ea0862abc5bf6fec814d8bf2
andy <andy.zhangtao@hotmail.com>
2017-12-05 15:01:26 +0800

    提交MakeDog代码
    Commit by Andy.Zhang
    Email: ztao8607@gmail.com

---
+++ Makefile
@@ -0,0 +1,14 @@
+
+.PHONY: build
+name = makeDog
+
+build:
+	go build -ldflags "-X main._VERSION_=$(shell date +%Y%m%d)" -o $(name)
+
+run: build
+	./$(name)
+
+release: *.go *.md
+	GOOS=linux GOARCH=amd64 go build -ldflags "-X main._VERSION_=$(shell date +%Y%m%d)" -a -o $(name)
+	docker build -t vikings/$(name) .
+	docker push vikings/$(name)
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"text/template"
+	"os"
+)
+
+const makefile = `
+.PHONY: build
+name = {{.Name}}
+
+build:
+	go build -ldflags "-X main._VERSION_=$(shell date +%Y%m%d)" -o $(name)
+
+run: build
+	./$(name)
+
+release: *.go *.md
+	GOOS=linux GOARCH=amd64 go build -ldflags "-X main._VERSION_=$(shell date +%Y%m%d)" -a -o $(name)
+	docker build -t vikings/$(name) .
+	docker push vikings/$(name)
+`
+
+var _VERSION_ = "unknown"
+
+var name = flag.String("name", "", "The binary name")
+
+func main() {
+	fmt.Println(getVersion())
+
+	flag.Parse()
+
+	if *name == "" {
+		fmt.Println("Name can't be empty! See MakeDog Usage below\n")
+		flag.Usage()
+		os.Exit(-1)
+	}
+
+	type MakeFile struct {
+		Name string
+	}
+
+	var mf = MakeFile{Name: *name}
+
+	t := template.Must(template.New("makefile").Parse(makefile))
+
+	file, err := os.Create("Makefile")
+	if err != nil{
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+
+	t.Execute(file, mf)
+}
+
+func getVersion() string {
+	return fmt.Sprintf("== MakeDog [%s] == \n", _VERSION_)
+}
